49dd650e77ca3debdab96695ebe6fcf6e348e884
Vinay R <rvinay343@gmail.com>
2021-06-24 17:30:24 +0530

    commit 2

--- qmi_tests/test_qmi.py
+++ qmi_tests/test_qmi.py
@@ -1,19 +1,18 @@
 import sys
+sys.path.append("..")
 from mrc_scripts.test_efs_backup import Test_backup
 
 class TestQmi:
 
     def __init__(self, dev_id):
-
         self.dev_id = dev_id
 
     def test_qmi_tests(self):
-
-        fin_out = qmi_obj.test_backup_main_method()
+        fin_out = backup_obj.test_backup_main_method()
         print(fin_out)
 
 #-------- Execution begins here ---------#
 dev_id = sys.argv[1]
-
-qmi_obj = Test_backup(dev_id)
-
+backup_obj = Test_backup(dev_id)
+qmi_obj = TestQmi(dev_id)
+qmi_obj.test_qmi_tests()
